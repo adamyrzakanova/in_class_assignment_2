dd07d334f10ba548bd8213fe0f8c132e0a565853
adamyrzakanova <aidaimyrzakanova@usf.edu>
2025-02-11 14:25:02 -0500

    Task 1 - Product Inventory Management.

---
+++ cc_example.js
@@ -0,0 +1,9 @@
+// Task 1
+
+let products = [
+    { name: "Laptop", price: 1200, category: "Electronics" },
+    { name: "Phone", price: 800, category: "Electronics" },
+    { name: "Desk Cjair", price: 150, category: "Furniture" },
+    { name: "Notebook", price: 5, category: "Stationery" },
+    { name: "Headphones", price: 100, category: "Electronics" },
+]; 
